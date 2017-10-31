d79a0c064bb4b9ad2fb8a62bd4622f2821802f2b
Rick Morgan <rmorgan@claimpilot.com>
2017-10-31 16:49:36 -0500

    removed alot of lines

--- GitTest/Class1.cs
+++ GitTest/Class1.cs
@@ -2,29 +2,7 @@
 {
     public class Class1
     {
-        //this is my test commit
-        //this is a new line
-        //oops that was a bad line
-        
-        // now let's try a new line
-        // and another new line
-        
-        // so where are we at with this test?
-        //  let's write some new lines and see.
-        
-        // now make a new commit and push
-        
-        //what is going on with "origin"
-        //add a new line before pushing
-        
-        //new line plus push
-        
-        //change pre-commit
-        
-        //change post-commit
-        
-        //change post-post-commit
-        
+     
         // so, now lets start making some changes
         // first let's remove some lines
     }
